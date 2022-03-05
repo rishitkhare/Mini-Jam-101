5a0efbb4bfbe8910bb0cb7f454c17a6ea2189d7d
Rishit Khare <kharerishit@gmail.com>
2022-03-05 15:13:36 -0600

    particles & coyote time

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,11 +3,11 @@ extends KinematicBody2D
 const HORIZONTAL_ACCEL = 20
 const HORIZONTAL_DECEL = 0.8
 const GRAVITY = 6
-const TERMINAL_FALL_VEL = 120
+const TERMINAL_FALL_VEL = 170
 
-const COYOTE_TIME_FRAMES = 20
+const COYOTE_TIME_FRAMES = 10
 
-const PRE_JUMP_LEEWAY = 20
+const PRE_JUMP_LEEWAY = 10
 
 const JUMP_POWER = -150
 
@@ -18,6 +18,9 @@ onready var frames_since_last_jump_press : int = 1000
 onready var frames_since_last_grounded : int = 1000
 
 onready var shape : CollisionShape2D = $CollisionShape2D
+
+onready var jumpParticles : CPUParticles2D = $Dust
+onready var landParticles : CPUParticles2D = $Dust2
 
 onready var rc_up : RayCast2D = $Raycasts/Up
 onready var rc_down : RayCast2D = $Raycasts/Down
@@ -40,10 +43,12 @@ func _physics_process(_delta):
 	if(Input.is_key_pressed(KEY_Z)):
 		apply_wind(Vector2(14, 0))
 	
+		
+	count_frames()
+	
 	do_x_movement(input)
 	
 	do_y_movement(input)
-
 
 func get_input() -> Vector2:
 	var input = Vector2()
@@ -72,19 +77,39 @@ func do_y_movement(_input : Vector2) -> void:
 	if(velocity.y > TERMINAL_FALL_VEL):
 		velocity.y = TERMINAL_FALL_VEL
 	
-	grounded = rc_down.is_colliding() || rc_downleft.is_colliding() || rc_downright.is_colliding()
+	var new_grounded = rc_down.is_colliding() || rc_downleft.is_colliding() || rc_downright.is_colliding()
+	
+	if(new_grounded && !grounded):
+		landParticles.restart()
+	
+	grounded = new_grounded
 	
 	if(grounded):
-		if(Input.is_action_just_pressed("ui_accept")):
-			velocity.y = JUMP_POWER
-		else:
-			velocity.y = 0
+		velocity.y = 0
+		frames_since_last_grounded = 0
+	else:
+		frames_since_last_grounded += 1
+		if(frames_since_last_grounded > 2048):
+			frames_since_last_grounded = 2048
 	
+	if(frames_since_last_grounded < COYOTE_TIME_FRAMES):
+		if(frames_since_last_jump_press < PRE_JUMP_LEEWAY):
+			jumpParticles.restart()
+			velocity.y = JUMP_POWER
+			frames_since_last_grounded = 2048
+			frames_since_last_jump_press = 2048
 	
 	# warning-ignore:return_value_discarded
 	move_and_slide(Vector2(0, velocity.y))
 		
 	
+func count_frames() -> void:
+	if(Input.is_action_just_pressed("ui_accept")):
+		frames_since_last_jump_press = 0
+	else:
+		frames_since_last_jump_press += 1
+		if(frames_since_last_jump_press > 2048):
+			frames_since_last_jump_press = 2048
 
 
 func apply_wind(direction : Vector2):
