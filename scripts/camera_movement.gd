812dd87e84a9d94961cd8da1bc952762869086db
Rishit Khare <kharerishit@gmail.com>
2022-03-05 03:27:26 -0600

    wind mechanic

--- scripts/camera_movement.gd
+++ scripts/camera_movement.gd
@@ -1,8 +1,17 @@
 extends Camera2D
 
 
-export var follow_factor : float = 0.05
+export var follow_factor_x : float = 0.05
+export var follow_factor_y : float = 0.07
+
+onready var unrounded_position = position
 
 
-func _process(delta):
-	position += (GameManager.get_player_position() - position) * follow_factor
+func _process(_delta):
+	$CPUParticles2D.emitting = Input.is_key_pressed(KEY_Z)
+		
+	
+	unrounded_position.x += (GameManager.get_player_position().x - unrounded_position.x) * follow_factor_x
+	unrounded_position.y += (GameManager.get_player_position().y - unrounded_position.y) * follow_factor_y
+	
+	position = Vector2(round(unrounded_position.x), round(unrounded_position.y))
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,27 +1,44 @@
 extends KinematicBody2D
 
-const TIME_FACTOR = 5
-
-const HORIZONTAL_ACCEL = 5
+const HORIZONTAL_ACCEL = 20
 const HORIZONTAL_DECEL = 0.8
-const GRAVITY = 0.3
-const TERMINAL_FALL_VEL = 25
+const GRAVITY = 6
+const TERMINAL_FALL_VEL = 120
 
-const JUMP_POWER = -30
+const COYOTE_TIME_FRAMES = 20
+
+const PRE_JUMP_LEEWAY = 20
+
+const JUMP_POWER = -150
 
 onready var velocity : Vector2 = Vector2()
 onready var grounded : bool = false
 
+onready var frames_since_last_jump_press : int = 1000
+onready var frames_since_last_grounded : int = 1000
+
+onready var shape : CollisionShape2D = $CollisionShape2D
+
 onready var rc_up : RayCast2D = $Raycasts/Up
 onready var rc_down : RayCast2D = $Raycasts/Down
+onready var rc_downleft : RayCast2D = $Raycasts/Downleft
+onready var rc_downright : RayCast2D = $Raycasts/Downright
+
 onready var rc_left : RayCast2D = $Raycasts/Left
 onready var rc_right : RayCast2D = $Raycasts/Right
 
 func _ready():
 	GameManager.register_player(self)
 	
+	
+func _draw():
+	draw_rect(Rect2(-shape.shape.extents, shape.shape.extents * 2), Color.aqua, false)
+	
 func _physics_process(_delta):
 	var input : Vector2 = get_input()
+	
+	if(Input.is_key_pressed(KEY_Z)):
+		apply_wind(Vector2(14, 0))
 	
 	do_x_movement(input)
 	
@@ -47,23 +64,28 @@ func do_x_movement(input : Vector2) -> void:
 	velocity.x *= HORIZONTAL_DECEL
 	
 # warning-ignore:return_value_discarded
-	move_and_slide(Vector2(velocity.x, 0) * TIME_FACTOR)
+	move_and_slide(Vector2(velocity.x, 0))
 	
 func do_y_movement(_input : Vector2) -> void:
-	velocity.y += 1
+	velocity.y += GRAVITY
 	
 	if(velocity.y > TERMINAL_FALL_VEL):
 		velocity.y = TERMINAL_FALL_VEL
 	
-	grounded = rc_down.is_colliding()
+	grounded = rc_down.is_colliding() || rc_downleft.is_colliding() || rc_downright.is_colliding()
 	
 	if(grounded):
-		velocity.y = 0
-		
-		if(Input.is_action_pressed("ui_accept")):
+		if(Input.is_action_just_pressed("ui_accept")):
 			velocity.y = JUMP_POWER
-			
-			
+		else:
+			velocity.y = 0
+	
+	
 	# warning-ignore:return_value_discarded
-	move_and_slide(Vector2(0, velocity.y) * TIME_FACTOR)
+	move_and_slide(Vector2(0, velocity.y))
+		
+	
 
+
+func apply_wind(direction : Vector2):
+	velocity += direction
